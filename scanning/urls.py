927b338fbdea0cd04a67359dc532c47a04380aca
Hilman Sijabat <hilman4452@gmail.com>
2023-03-30 22:08:06 +0700

    Tampilan udah siap, tapi menghubungkan belum

--- scanning/urls.py
+++ scanning/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('', views.index),
-    path("scanning", views.scanning_request),
+    path('scanning/', views.scanning_request, name='scanning'),
+    # path('scanning', views.detail),
 ]
--- scanning/views.py
+++ scanning/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
+from django.conf import settings
+import os
 
 
 # Create your views here.
 
 # TODO: Create view for index
 def index(request):
-    context = {
-        "name": "Andree Panjaitan",
-    }
+    context = {}
     return render(request, 'scanning/index.html', context)
-
 
 def scanning_request(request):
     context = {}
     return render(request, 'scanning/scanning.html', context)
+
+# def detail(request):
+#     context = {}
+#     return render(request, 'scanning/detail.html', context)
